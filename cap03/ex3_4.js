5b614dc05f069c64ae95dd4d0a45faf805608839
Ronald Pinheiro <109386027+pinheiroronald@users.noreply.github.com>
2023-01-22 23:48:28 -0300

    atu

--- cap03/ex3_4.js
+++ cap03/ex3_4.js
@@ -1,3 +1,8 @@
 const prompt = require("prompt-sync")()
 const pesokg = Number(prompt("Peso da ração (kg): "))
-const consumo = Number(prompt(prompt("")))
+const consumo = Number(prompt("Consumo diário (gr): "))
+const pesoGr = pesokg * 1000
+const duracao = Math.floor(pesoGr/consumo)
+const sobra = pesoGr % consumo
+console.log(`Duração: ${duracao} dias`)
+console.log(`sobra: ${sobra}gr`)
---
+++ cap04/js/ex4_1.js
@@ -0,0 +1,3 @@
+const frm = window.document.querySelector("form")
+const resp1 = window.document.querySelector("res1")
+const resp2 = window.document.querySelector("res2")
