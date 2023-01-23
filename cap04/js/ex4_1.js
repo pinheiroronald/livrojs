8b327e41a0ed6f26c77006a931d3cbecdab47459
Ronald Pinheiro <109386027+pinheiroronald@users.noreply.github.com>
2023-01-23 17:49:39 -0300

    sub

--- cap04/js/ex4_1.js
+++ cap04/js/ex4_1.js
@@ -1,3 +1,24 @@
 const frm = window.document.querySelector("form")
-const resp1 = window.document.querySelector("res1")
-const resp2 = window.document.querySelector("res2")
+const resp1 = window.document.querySelector("h3")
+const resp2 = window.document.querySelector("h4")
+
+frm.addEventListener("submit", (e)=>{
+    const nome = frm.inNome.value
+    const not1 = Number(frm.inNota1.value)
+    const not2 =Number(frm.inNota2.value)
+    
+    const media = (not1 + not2)/ 2
+
+    if(media >= 7 && not1 >=7 && not2 >= 7){
+        resp2.innerText=`Parabéns ${nome} você foi aprovado(a).`
+        resp2.style.color ='blue'
+    }else if(media >= 4){
+        resp2.innerText= `Atenção ${nome} você esta em exame.`
+        resp2.style.color='green'
+    }else{
+        resp2.innerText=`Ops!, ${nome} você foi reprovad(a)`
+        resp2.style.color='red'
+    }
+    resp1.innerText=`Media das notas: ${media.toFixed(1)}`
+    e.preventDefault()
+})
