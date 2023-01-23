7d35f791ca3e3077f25ddfa18e0a8136fe6313c8
Ronald Pinheiro <109386027+pinheiroronald@users.noreply.github.com>
2023-01-23 19:04:20 -0300

    sdd

---
+++ cap04/js/ex4_2.js
@@ -0,0 +1,17 @@
+const frm = window.document.querySelector("form")
+const resp = window.document.querySelector("h3")
+
+frm.addEventListener("submit",(e)=>{
+    const nome = frm.inNome.value
+    const masculino = frm.inMasculino.checked
+    const altura = Number(frm.inAltura.value)
+    let peso
+    if(masculino){
+        peso = 22 * Math.pow(altura, 2)
+    }else{
+        peso = 21 * Math.pow(altura, 2)
+    }
+    resp.innerText=`${nome}: Seu pesso Ideal é ${peso.toFixed(3)}kg`
+   
+ e.preventDefault()
+})
