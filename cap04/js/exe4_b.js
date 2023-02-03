2f31ff906b3946ed65217b9eb6cd1dd250274446
Ronald Pinheiro <109386027+pinheiroronald@users.noreply.github.com>
2023-02-03 10:49:01 -0300

    atu

--- cap04/js/exe4_b.js
+++ cap04/js/exe4_b.js
@@ -1,11 +1,19 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-frm.addEventListener("Submit",(t)=>{
-    t.preventDefault()
-    const veloPer = Number(frm.inVelPermit.value)
-    const velPerm = Number(frm.inVelCondu.value)
-     if(){
-        
+frm.addEventListener("Submit",(e)=>{
+    e.preventDefault()
+    const velPer = Number(frm.inVelPermit.value)
+    const velCon = Number(frm.inVelCondu.value)
+
+     if(velCon <= velPer){
+       resp.innerText="Situação: Sem Multa." 
+     }else{
+        const maisVinte = velPer * 1.2
+        if(velCon <= maisVinte){
+          resp.innerText="Situação: Multa leve."
+        }else{
+          resp.innerText="Situação: Multa Pessada"
+        }
      }
 })
